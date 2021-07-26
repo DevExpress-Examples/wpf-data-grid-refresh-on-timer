24ba615373095eea3e382cf7db68c731c8d8f8a9
alexander_ploskin <alexander.ploskin@devexpress.com>
2021-07-26 16:07:58 +0300

    Make refresh on timer collection

---
+++ RefreshOnTimer/RefreshOnTimerCollection.cs
@@ -0,0 +1,74 @@
+﻿using DevExpress.Xpf.Grid;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+using System.Collections.Specialized;
+using DevExpress.Data.Helpers;
+using DevExpress.Data.Filtering.Helpers;
+
+
+namespace WpfApp6 {
+    public class RefreshOnTimerCollection : IEnumerable, INotifyCollectionChanged {
+        protected static IBindingList GetListSource(object source, bool prioritizeIListSource) {
+            if(source is IListSource && (prioritizeIListSource || !(source is IEnumerable)))
+                return ExtractListSource(((IListSource)source).GetList());
+            return ExtractListSource(source as IEnumerable);
+        }
+        static IBindingList ExtractListSource(IEnumerable source) {
+            if(source == null)
+                return null;
+            var list = source as IList;
+            if(list == null) {
+                list = new EnumerableObservableWrapper<object>((IEnumerable)source);
+            }
+            if(list is IBindingList) {
+                return (IBindingList)list;
+            }
+            return BindingListAdapterBase.CreateFromList(list, ItemPropertyNotificationMode.PropertyChanged);
+        }
+
+        public RefreshOnTimerCollection(TimeSpan interval, object dataSource) {
+            timer = new DispatcherTimer(DispatcherPriority.Background);
+            timer.Interval = interval;
+            timer.Tick += OnTick;
+            timer.Start();
+
+            storage = GetListSource(dataSource, false);
+            storage.ListChanged += Storage_ListChanged;
+        }
+
+        private void Storage_ListChanged(object sender, ListChangedEventArgs e) {
+            changed = true;
+        }
+
+        bool changed;
+
+        void OnTick(object sender, EventArgs eventArgs) {
+            if(changed) {
+                OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+            }
+
+            changed = false;
+        }
+
+        DispatcherTimer timer;
+
+        private IBindingList storage;
+
+        private void OnCollectionChanged(NotifyCollectionChangedEventArgs e) {
+            CollectionChanged?.Invoke(this, e);
+        }
+
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            return storage.GetEnumerator();
+        }
+    }
+}
---
+++ RefreshOnTimer/ViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using DevExpress.Mvvm;
+using DevExpress.Xpf.Grid;
+
+namespace WpfApp6 {
+    class ViewModel {
+
+        static object SyncRoot = new object();
+
+        Timer timer;
+
+        int counter = 1;
+
+        protected void TimerCallback(object state) {
+            lock(SyncRoot) {
+                counter++;
+                storage.Add(new DataItem() { Id = counter, Name = "A" });
+            }
+        }
+
+        public ViewModel() {
+            storage = new ObservableCollection<DataItem>() { new DataItem() { Id = 1, Name = "A" } };
+            timer = new Timer(TimerCallback, null, 1000, 1000);
+            Source = new RefreshOnTimerCollection(TimeSpan.FromSeconds(3), storage);
+            AddNew = new DelegateCommand(() => {
+                counter++;
+                storage.Add(new DataItem() { Id = counter, Name = "A" });
+            });
+            EditFirst = new DelegateCommand(() => {
+                storage[0].Name = "Edited";
+            });
+        }
+
+        /// <summary>
+        /// Customer's collection to perform changes
+        /// </summary>
+        private ObservableCollection<DataItem> storage;
+
+        /// <summary>
+        /// Refresh on timer collection to be bound 
+        /// </summary>
+        public RefreshOnTimerCollection Source { get; set; }
+
+        public DelegateCommand AddNew { get; }
+
+        public DelegateCommand EditFirst { get; }
+    }
+}
